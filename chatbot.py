00d4665daba09375fd754129657f599bf9beae06
aziz.seyah@yahoo.fr <aziz.seyah@yahoo.fr>
2022-01-16 13:31:28 +0100

    initial commit

---
+++ chatbot.py
@@ -0,0 +1,186 @@
+import keras
+import nltk
+nltk.download('omw-1.4')
+import tensorflow as tf 
+import pickle
+import json
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense,Dropout,Activation
+import random
+import datetime
+import webbrowser
+import requests
+import time
+from pygame import mixer
+import COVID19Py
+from nltk.stem import WordNetLemmatizer
+lemmatizer=WordNetLemmatizer()
+
+words=[]
+classes = []
+documents = []
+ignore_words = ['?', '!']
+data_file = open('data.json').read()
+intents = json.loads(data_file)
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        #tokenize chaque mot
+        w = nltk.word_tokenize(pattern)
+        words.extend(w)
+        #ajouter des documents
+        documents.append((w, intent['tag']))
+        # ajout dans classes
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+# lemmaztize and lower each word and remove duplicates
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+
+# sort classes
+classes = sorted(list(set(classes)))
+
+# documents = combination between patterns and intents
+print (len(documents), "documents")
+
+# classes = intents
+print (len(classes), "classes", classes)
+
+# words = all words, vocabulary
+print (len(words), "unique lemmatized words", words)
+pickle.dump(words,open('texts.pkl','wb'))
+pickle.dump(classes,open('labels.pkl','wb'))
+
+# create our training data
+training = []
+
+# create an empty array for our output
+output_empty = [0] * len(classes)
+
+# training set, bag of words for each sentence
+for doc in documents:
+
+    # initialize our bag of words
+    bag = []
+    # list of tokenized words for the pattern
+    pattern_words = doc[0]
+    # lemmatize each word - create base word, in attempt to represent related words
+    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+    # create our bag of words array with 1, if word match found in current pattern
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+    
+    # output is a '0' for each tag and '1' for current tag (for each pattern)
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+    
+    training.append([bag, output_row])
+# shuffle our features and turn into np.array
+random.shuffle(training)
+training = np.array(training)
+# create train and test lists. X - patterns, Y - intents
+X_train = list(training[:,0])
+y_train = list(training[:,1])
+print("Training data created")
+
+
+#Model
+model=Sequential()
+model.add(Dense(128,activation='relu',input_shape=(len(X_train[0]),)))
+model.add(Dropout(0.5))
+model.add(Dense(64,activation='relu'))
+model.add(Dense(64,activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(y_train[0]),activation='softmax'))
+
+adam=tf.optimizers.Adam(0.001)
+model.compile(optimizer=adam,loss='categorical_crossentropy',metrics=['accuracy'])
+#model.fit(np.array(X_train),np.array(y_train),epochs=200,batch_size=10,verbose=1)
+weights=model.fit(np.array(X_train),np.array(y_train),epochs=200,batch_size=10,verbose=1)    
+model.save('mymodel.h5',weights)
+
+
+import nltk
+nltk.download('popular')
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import pickle
+import numpy as np
+from keras.models import load_model
+model = load_model('mymodel.h5')
+import json
+import random
+intents = json.loads(open('data.json').read())
+words = pickle.load(open('texts.pkl','rb'))
+classes = pickle.load(open('labels.pkl','rb'))
+
+def clean_up_sentence(sentence):
+    # tokenize the pattern - split words into array
+
+    sentence_words = nltk.word_tokenize(sentence)
+    # stem each word - create short form for word
+
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+
+def bow(sentence, words, show_details=True):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0]*len(words)  
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s: 
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+def predict_class(sentence, model):
+    # filter out predictions below a threshold
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+    
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    if tag=='datetime':        
+        print(time.strftime("%A"))
+        print (time.strftime("%d %B %Y"))
+        print (time.strftime("%H:%M:%S"))
+    return result
+
+
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
+    res = getResponse(ints, intents)
+    return res
+
+#routes
+from flask import Flask, render_template, request
+app = Flask(__name__)
+app.static_folder = 'static'
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return chatbot_response(userText)
+if __name__ == "__main__":
+    app.run()
